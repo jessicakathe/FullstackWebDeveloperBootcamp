29dfcbeed357d1c5ee7252a87f5af485e0f0747c
Jessica Franco <jessica_kathe@hotmail.com>
2024-04-22 17:32:47 -0500

    Modulo III: Lab1

---
+++ Tarea3/lab1/lab1.js
@@ -0,0 +1,6 @@
+
+/**  Comentario para documentación
+ *  @author Jessica Franco <jessicakathe@gmail.com>
+ *  @fileoverview This script use variable declarations and comments
+ *  @license BSD 3-clause License
+*/
